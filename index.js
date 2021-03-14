423bf3cd40406201d8fbe658d47e7e3726222e17
Justin <jreed8910@gmail.com>
2021-03-14 13:35:24 -0400

    function creations

--- index.js
+++ index.js
@@ -5,3 +5,35 @@ age=31;//assignment statement
 console.log(age);
 var height=6;
 console.log(height);
+var pressure=205;
+var temp = 60;
+if(pressure>200) {
+    console.log("big trouble");
+}
+else if(pressure>150) {
+    // '' === ""
+    console.log('attention needed lets be proactive');
+}
+else {
+    console.log("normal operation")
+}
+// AND operator &&
+if (pressure > 200 && temp > 100) {
+
+}
+// OR operator ||
+if (pressure > 200 || temp < 20) {
+    // do this when the pressure is greator than 200 or the temp less than 20
+}
+
+// <, >, <=, >=, ===
+if (pressure === 100) {
+    //do this if the pressure is exactly 100 
+}
+
+function convertToCelcius(tempInFarenheit) {
+    return (tempInFarenheit - 32) * (5/9);
+}
+
+var degreesInCelcius = convertToCelcius(212);
+console.log(degreesInCelcius);
